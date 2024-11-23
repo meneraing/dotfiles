9950519072948e90b2bdb346d6b50bcb05a49fed
meneraing <meneraing@gmail.com>
2024-11-22 22:03:56 -0300

    Update waybar look

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -6,6 +6,7 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.prettier,
       },
     })
     vim.keymap.set("n", "<leader>ft", vim.lsp.buf.format, {})
